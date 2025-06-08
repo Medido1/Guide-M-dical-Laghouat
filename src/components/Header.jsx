f72a26ac53ba95d507595f8799813abc70311e2b
Medido1 <medi1222@gmail.com>
2025-06-08 18:34:49 +0100

    Add description to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,16 @@ import { GiMedicalDrip } from "react-icons/gi";
 
 function Header() {
   return (
-    <header className="bg-blue-200 p-4 flex gap-2 items-center justify-center shadow-lg">
-      <GiMedicalDrip />
-      <h1 className="text-xl font-bold">
-        Guide Médical Laghouat
-      </h1>
+    <header className="bg-blue-200 p-4 flex flex-col gap-2 items-center justify-center shadow-lg">
+      <div className="flex gap-2 items-center">
+        <GiMedicalDrip />
+        <h1 className="text-2xl font-bold">
+          Guide Médical Laghouat
+        </h1>
+      </div>
+      <p className="text-sm text-center">
+        Trouvez facilement les laboratoires, centres d’imagerie et médecins à Laghouat. 
+      </p>
     </header>
   )
 }
