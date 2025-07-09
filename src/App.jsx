5baecf1737666a939d7583c5a7f86a361a69e9a2
Medido1 <medi1222@gmail.com>
2025-07-09 22:30:15 +0100

    Add lazy loading to improve performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import './App.css'
 import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import { lazy, Suspense } from 'react';
+const Main = lazy(() => import('./components/Main'))
+const Footer = lazy(() => import('./components/Footer'))
 
 function App() {
 
   return (
-    <>
+    <Suspense fallback={<div className="text-center py-4">Chargement…</div>}>
      <div className='min-h-screen flex flex-col'>
        <Header />
        <Main />
        <Footer />
      </div>
-    </>
+    </Suspense>
   )
 }
 
